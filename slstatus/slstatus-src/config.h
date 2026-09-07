849fe250947b9b7543b304ce9fb74ec19062ff0f
Clara Knight <cea144@ucla.edu>
2026-09-07 16:38:33 -0700

    Revert "Revert "Updated battery display""
    This reverts commit 3b7b6aa55886a8c22dde200f777e752c8b540fd4.

--- slstatus/slstatus-src/config.h
+++ slstatus/slstatus-src/config.h
@@ -25,43 +25,48 @@ static const char unknown_str[] = "n/a";
 static const char *colored_battery(const char *bat) {
   const char *perc_str = battery_perc(bat);
   if (!perc_str)
-    return NULL;
+    return "";
 
   int perc = atoi(perc_str);
+
+  const char *perc_color = "^c7^";
   if (perc < 20)
-    return bprintf(RED "%d%%", perc);
+    perc_color = RED;
   else if (perc < 40)
-    return bprintf(ORANGE "%d%%", perc);
-  else
-    return bprintf("^c7^%d%%", perc);
-}
+    perc_color = ORANGE;
 
-static const char *colored_battery_state(const char *bat) {
   const char *state_str = battery_raw_state(bat);
-  if (!state_str)
-    return NULL;
   const char *state_sym = battery_state(bat);
-  if (!state_sym)
-    return NULL;
+  const char *state_color = "";
 
-  if (strcmp(state_str, "Full") == 0)
-    return bprintf(BLUE "%s^d^", state_sym);
-  else if (strcmp(state_str, "Charging") == 0)
-    return bprintf(GREEN "%s^d^", state_sym);
-  else if (strcmp(state_str, "Discharging") == 0)
-    return bprintf(YELLOW "%s^d^", state_sym);
+  if (state_str) {
+    if (strcmp(state_str, "Full") == 0)
+      state_color = BLUE;
+    else if (strcmp(state_str, "Charging") == 0)
+      state_color = GREEN;
+    else if (strcmp(state_str, "Discharging") == 0)
+      state_color = YELLOW;
+  }
 
-  return bprintf("%s^d^", state_sym);
+  const char *rem = battery_remaining(bat);
+  char rem_str[64] = "";
+  if (rem && *rem) {
+    snprintf(rem_str, sizeof(rem_str), " ^c7^(%s)^d^", rem);
+  }
+
+  return bprintf("^c6^|^d^ ^c7^" BATSYM "^d^ %s%d%%^d^ %s%s^d^%s",
+                 perc_color, perc,
+                 state_color, state_sym ? state_sym : "",
+                 rem_str);
 }
 
 static const struct arg args[] = {
     /* function        format                               argument */
-    {colored_battery, "^c6^|^d^ ^c7^🔋^d^ %s^d^", "BAT0"},
-    {colored_battery_state, " %s^d^", "BAT0"},
-    {battery_remaining, " ^c7^(%s)^d^", "BAT0"},
-    {ram_perc, " ^c2^|^d^  ^c7^%s%%^d^", NULL},
-    {datetime, " ^c3^|^d^ ^c7^%s^d^", "%a %m/%d/%Y"},
+    {colored_battery,  "%s",                                "BAT0"},
+    {ram_perc,         " ^c2^|^d^  ^c7^%s%%^d^",            NULL},
+    {datetime,         " ^c3^|^d^ ^c7^%s^d^",               "%a %m/%d/%Y"},
     //{run_command, " ^c7^(%s)^d^",
     // "sh -c 'echo Winter 2026: $(($(date +%W) - 0))/10'"},
-    {datetime, " ^c1^|^d^ ^c7^%s^d^ ", "%H:%M:%S"},
+    {datetime,         " ^c1^|^d^ ^c7^%s^d^ ",              "%H:%M:%S"},
 };
+
