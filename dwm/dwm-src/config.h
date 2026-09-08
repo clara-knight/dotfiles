fa8bd6f61e2448d26dfa3e0731856f72cd0bd11f
Clara Knight <cea144@ucla.edu>
2026-09-08 11:58:20 -0700

    added workspaces  to dwm

--- dwm/dwm-src/config.h
+++ dwm/dwm-src/config.h
@@ -25,7 +25,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3"};
+static const char *tags[] = {"1", "2", "3", "4", "5"};
 
 static const Rule rules[] = {
     /* xprop(1):
